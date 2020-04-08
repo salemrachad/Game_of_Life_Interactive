129ed5bcb4e17eb5d94ffc16cc40068d0599bea5
salemrachad <61739179+salemrachad@users.noreply.github.com>
2020-04-08 23:57:16 +0200

    wifi-weather implementation

---
+++ arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "Network"
+#define SECRET_PASS "Password"
--- gol_arduino_code.ino
+++ gol_arduino_code.ino
@@ -1,4 +1,25 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Wifi Setup
+
+#include <WiFiNINA.h>
+#include <ArduinoJson.h>
+#include "arduino_secrets.h"
+#include <stdio.h>
+#include <SPI.h>
+
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+
+// Initialize the Wifi client library
+WiFiSSLClient client;
+
+unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 30L * 1000L; // delay between updates, in milliseconds
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <Adafruit_NeoPixel.h>
 
 //DEFINE MATRIX 32x32
@@ -121,6 +142,10 @@ int gstate = 0;
 void setup() {
   //Start Serial
   Serial.begin(115200);
+
+  //Attempt to Connect to wifi
+  connect2Wifi();
+
   //Starting Dot Matrix
   pixels.begin();
   pixels.show();
@@ -133,11 +158,13 @@ void loop() {
   stateLedColor();
 
   switch (gstate) {
+
     case 0:
     pauseGame();
     pauseButton();
     resetButton();
     break;
+
     case 1:
     pixels.clear();
     for (int i = 0; i < WIDTH * HEIGHT; i++) {
@@ -145,9 +172,14 @@ void loop() {
     }
     pixels.show();
     break;
+
     case 2:
     //statement
     pixels.clear();
+    if (millis() - lastConnectionTime > postingInterval)
+  {
+    httpRequest();
+  }
     break;
   }
 }
@@ -367,7 +399,6 @@ void stateLedColor(){   //RGB LED color depending on gstate.
   }
 }
 
-
 void pauseGame() {
   if (millis() - lastRecordedTime > interval) {
     if (!pause) {
@@ -375,6 +406,157 @@ void pauseGame() {
       statePlot();
       lastRecordedTime = millis();
     }
+  }
+}
+
+void connect2Wifi(){
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < "1.0.0")
+  {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  // you're connected now, so print out the status:
+  printWifiStatus();
+  }
+}
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void httpRequest()
+{
+  // if there's a successful connection:
+  if (client.connect("api.openweathermap.org", 443))
+  {
+    Serial.println("connecting...");
+    // send the HTTP PUT request:
+    client.println("GET /data/2.5/weather?q=edinburgh,uk&units=metric&APPID=YOUR_API_KEY HTTP/1.1");
+    client.println("Host: api.openweathermap.org");
+    client.println("Connection: close");
+    client.println();
+
+    // Check HTTP status
+    char status[32] = {0};
+    client.readBytesUntil('\r', status, sizeof(status));
+    // It should be "HTTP/1.0 200 OK" or "HTTP/1.1 200 OK"
+    if (strcmp(status + 9, "200 OK") != 0)
+    {
+      Serial.print(F("Unexpected response: "));
+      Serial.println(status);
+      return;
+    }
+
+    // Skip HTTP headers
+    char endOfHeaders[] = "\r\n\r\n";
+    if (!client.find(endOfHeaders))
+    {
+      Serial.println(F("Invalid response"));
+      return;
+    }
+
+    // Allocate the JSON document
+    // Use arduinojson.org/v6/assistant to compute the capacity.
+    const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(0) + 2*JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + 2*JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(14) + 280;
+    DynamicJsonDocument doc(capacity);
+
+    // Parse JSON object
+    DeserializationError error = deserializeJson(doc, client);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
+    }
+
+    int weatherId = doc["weather"][0]["id"].as<int>();
+    float weatherTemperature = doc["main"]["temp"].as<float>();
+    int weatherHumidity = doc["main"]["humidity"].as<int>();
+
+    //Disconnect
+    client.stop();
+
+    Serial.println(F("Response:"));
+    Serial.print("Weather: ");
+    Serial.println(weatherId);
+    Serial.print("Temperature: ");
+    Serial.println(weatherTemperature);
+    Serial.print("Humidity: ");
+    Serial.println(weatherHumidity);
+    Serial.println();
+
+    // char scrollText[15];
+    // sprintf(scrollText, "Humidity:%3d%%", weatherHumidity);
+
+    if(weatherId == 800)    //clear
+    {
+      // draw(scrollText, SUN, weatherTemperature);
+    }
+    else
+    {
+      switch(weatherId/100)
+      {
+        case 2:     //Thunderstorm
+            // draw(scrollText, THUNDER, weatherTemperature);
+            break;
+
+        case 3:     //Drizzle
+        case 5:     //Rain
+            // draw(scrollText, RAIN, weatherTemperature);
+            break;
+
+        case 7:     //Sun with clouds
+            // draw(scrollText, SUN_CLOUD, weatherTemperature);
+            break;
+        case 8:     //clouds
+            // draw(scrollText, CLOUD, weatherTemperature);
+            break;
+
+        default:    //Sun with clouds
+            // draw(scrollText, SUN_CLOUD, weatherTemperature);
+            break;
+      }
+    }
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  }
+  else
+  {
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
   }
 }
 
