aeb5010e24b0facfae189dc0f25d9899727b38d2
rsalem <61739179+salemrachad@users.noreply.github.com>
2020-04-30 15:58:38 +0200

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_SSID "Pixel_2771"
-#define SECRET_PASS "1234567890"
-#define OPENWEATHER_API "GET /data/2.5/weather?q=spain,madrid&appid=4483727fd8bd14c96f505ab796963203 HTTP/1.1"
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define OPENWEATHER_API ""
